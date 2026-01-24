ddb969db2dd06b47969b8fe2e57eb641abeac171
Kozavochka <a.ermolov@sandbox.ru>
2026-01-24 11:40:15 +0300

    KPGrouping to 1 out_dim

--- models/KPGrouping.py
+++ models/KPGrouping.py
@@ -6,7 +6,7 @@ class Model(kp_group):
         n = 5
         dims = [256, 256, 384, 384, 384, 512]
         modules = [2, 2, 2, 2, 2, 4]
-        out_dim = 3
+        out_dim = 1
 
         super(Model, self).__init__(
             n, 2, dims, modules, out_dim,
@@ -17,4 +17,3 @@ class Model(kp_group):
 
 
 loss = GroupingLoss(focal_loss=_neg_loss, lambda_=4, lambda_b=2)  
-
